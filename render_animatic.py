b79c65a76db266b853132dd078a00c5c40961fb8
IRIE Shinsuke <irieshinsuke@yahoo.co.jp>
2019-08-01 13:45:50 +0900

    Bump version to 0.3.1

--- render_animatic.py
+++ render_animatic.py
@@ -23,7 +23,7 @@ bl_info = {
     "name": "Render Animatic",
     "description": "Render only keyframes or only frames pointed to by markers",
     "author": "IRIE Shinsuke",
-    "version": (0, 3, 0),
+    "version": (0, 3, 1),
     "blender": (2, 80, 0),  # or (2, 79, 0)
     "location": "Topbar > Render > Render Animatic",
     "tracker_url": "https://github.com/iRi-E/blender_render_animatic/issues",
